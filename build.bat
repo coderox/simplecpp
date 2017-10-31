f82cac573b1ff07186e20cb7dfc913d9f5d6a872
Johan Lindfors <Johan.Lindfors@coderox.se>
2017-10-31 09:45:48 +0100

    Now working better with Visual Studio 2017, need to test on separate computer and OSX

--- build.bat
+++ build.bat
@@ -1,3 +1,5 @@
 @echo off
-call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\Common7\Tools\VsDevCmd.bat"
-cl.exe %1
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" x64
+set compilerflags=/Od /Zi /EHsc
+set linkerflags=/OUT:%3/%2.exe
+cl.exe %compilerflags% %1 /link %linkerflags%
--- main.cpp
+++ main.cpp
@@ -1,5 +1,21 @@
-#include <stdio.h>
+#include <iostream>
 
-int main(){
-    printf("Hello world");
+class Base {
+    virtual void method() {std::cout << "from Base" << std::endl;}
+public:
+    virtual ~Base() {method();}
+    void baseMethod() {method();}
+};
+
+class A : public Base {
+    void method() {std::cout << "from A" << std::endl;}
+public:
+    ~A() {method();}
+};
+
+int main(void) {
+    Base* base = new A;
+    base->baseMethod();
+    delete base;
+    return 0;
 }
