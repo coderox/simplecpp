017801095fd63a2bb0df4553d643b9c37f84dadc
Johan Lindfors <johan.lindfors@coderox.se>
2017-10-31 10:18:53 +0100

    Better structure when building

--- build.bat
+++ build.bat
@@ -1,5 +1,6 @@
 @echo off
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" x64
+cd %3/build/
 set compilerflags=/Od /Zi /EHsc
-set linkerflags=/OUT:%3/%2.exe
+set linkerflags=/OUT:%3/output/%2.exe
 cl.exe %compilerflags% %1 /link %linkerflags%
